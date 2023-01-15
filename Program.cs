576efd12cfa6beefb671027c3e001df0fd401318
its julii <nanji828@gmail.com>
2023-01-15 10:30:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Time___15min
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            int time = (hours * 60 + minutes) + 15;
+            int hoursform = time / 60;
+            int minutesform = time % 60;
+
+            if (hoursform == 24)
+            {
+                Console.WriteLine($"0:{minutesform:d2}");
+            }
+            else
+            {
+                Console.WriteLine($"{hoursform}:{minutesform:d2}");
+            }
+        }
+    }
+}
